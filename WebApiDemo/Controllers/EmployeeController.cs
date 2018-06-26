c2f79de633b014802e89f1e80ae00e9aa1453d3a
mendax02 <siddharth.rawat@outlook.com>
2018-06-26 09:27:46 +0530

    Add project files.

---
+++ WebApiDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiDemo.Models;
+
+namespace WebApiDemo.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public IHttpActionResult PostNewEmployee(EmployeeModel employee)
+        {
+
+            if (!ModelState.IsValid)
+                return BadRequest("Invalid data.");
+
+            using (var ctx = new TrainingEntities())
+            {
+                ctx.Employees.Add(
+                    new Employee {
+                        EmployeeId = employee.EmployeeId,
+                        EmployeeName= employee.EmployeeName,
+                        EmployeeRole = employee.EmployeeRole,
+                        DepartmentId = employee.DepartmentId });
+                // employees.Add(employee);
+                ctx.SaveChanges();
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ WebApiDemo/Models/EmployeeModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiDemo.Models
+{
+    public class EmployeeModel
+    {
+        public string EmployeeName { get; set; }
+        public int EmployeeId { get; set; }
+        public string EmployeeRole { get; set; }
+        public Nullable<int> DepartmentId { get; set; }
+        //public ICollection<EmployeeModel> Employees { get; set; }
+    }
+}
